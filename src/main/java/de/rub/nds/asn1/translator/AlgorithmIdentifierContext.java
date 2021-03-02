3e9cf7564df9c65251ba650c46f9db6e9fa1332b
ic0ns <robert.merget@rub.de>
2021-03-02 17:21:34 +0100

    translated comment to english

--- src/main/java/de/rub/nds/asn1/translator/AlgorithmIdentifierContext.java
+++ src/main/java/de/rub/nds/asn1/translator/AlgorithmIdentifierContext.java
@@ -6,7 +6,6 @@
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-
 package de.rub.nds.asn1.translator;
 
 import de.rub.nds.asn1.translator.defaultcontextcomponentoptions.*;
@@ -15,18 +14,11 @@ public class AlgorithmIdentifierContext extends Context {
 
     public static String NAME = "AlgorithmIdentifierContext";
 
-    private static final ContextComponent[] contextComponents = new ContextComponent[] {
-        new ContextComponent("algorithm", "", new ContextComponentOption<?>[] { new Asn1ObjectIdentifierCCO() }, false,
-            false),
-        new ContextComponent("parameters", "", new ContextComponentOption<?>[] { new Asn1NullCCO() }, true, false) // TODO:
-                                                                                                                   // laut
-                                                                                                                   // RFC
-                                                                                                                   // Type
-                                                                                                                   // ANY,
-                                                                                                                   // hier
-                                                                                                                   // nur
-                                                                                                                   // NULL
-                                                                                                                   // abgedeckt!
+    private static final ContextComponent[] contextComponents = new ContextComponent[]{
+        new ContextComponent("algorithm", "", new ContextComponentOption<?>[]{new Asn1ObjectIdentifierCCO()}, false,
+        false),
+        new ContextComponent("parameters", "", new ContextComponentOption<?>[]{new Asn1NullCCO()}, true, false)
+// TODO according to rfc this only covers null
     };
 
     public AlgorithmIdentifierContext() {
