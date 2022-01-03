a10d97664925a22f1a5a9cfab27ea2f71a9eee93
JonSnowWhite <Niklas.Niere@ruhr-uni-bochum.de>
2022-01-03 16:32:29 +0100

    Updated license headers

--- src/test/java/de/rub/nds/x509attacker/x509/serializer/X509CertificateSerializerTest.java
+++ src/test/java/de/rub/nds/x509attacker/x509/serializer/X509CertificateSerializerTest.java
@@ -1,7 +1,7 @@
 /**
  * X.509-Attacker - A tool for creating arbitrary certificates
  *
- * Copyright 2014-2021 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ * Copyright 2014-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
