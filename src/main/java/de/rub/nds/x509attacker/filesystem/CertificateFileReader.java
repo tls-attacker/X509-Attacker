8881713532437096a34ebc93473562a25c5f5af1
Robert Merget <robert.merget@tii.ae>
2022-12-29 16:56:02 +0400

    added file reader

--- src/main/java/de/rub/nds/x509attacker/filesystem/CertificateFileReader.java
+++ src/main/java/de/rub/nds/x509attacker/filesystem/CertificateFileReader.java
@@ -8,10 +8,16 @@
  */
 package de.rub.nds.x509attacker.filesystem;
 
+import de.rub.nds.x509attacker.x509.base.X509Certificate;
+import de.rub.nds.x509attacker.x509.base.X509CertificateChain;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.function.Consumer;
 
 public class CertificateFileReader {
 
@@ -19,31 +25,41 @@ public class CertificateFileReader {
 
     public static final String CERTIFICATE_PEM_SUFFIX = "-----END CERTIFICATE-----";
 
-    private final File file;
-
-    public CertificateFileReader(final File file) {
-        this.file = file;
+    private CertificateFileReader() {
     }
 
-    public byte[] readBytes() throws IOException {
-        FileInputStream stream = new FileInputStream(file);
-        byte[] bytes = stream.readAllBytes();
-        String certificateFileContent = new String(bytes);
-        if (certificateFileContent.contains(CERTIFICATE_PEM_PREFIX)
-                && certificateFileContent.contains(CERTIFICATE_PEM_SUFFIX)) {
-            String base64Str =
-                    certificateFileContent
-                            .replace("\\n", "")
-                            .replace("\n", "")
-                            .replace("\r", "")
-                            .replace(CERTIFICATE_PEM_PREFIX, "")
-                            .replace(CERTIFICATE_PEM_SUFFIX, "")
-                            .trim();
-            bytes = Base64.getDecoder().decode(base64Str);
-        }
-        if (bytes == null) {
-            throw new IOException("File is not a PEM-encoded certificate file!");
-        }
-        return bytes;
+    public static X509CertificateChain readBytes(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        X509CertificateChain chain = new X509CertificateChain();
+        reader.lines().forEach(new Consumer<String>() {
+            private ByteArrayOutputStream stream = null;
+
+            @Override
+            public void accept(String line) {
+                if (line.contains(CERTIFICATE_PEM_PREFIX)) {
+                    stream = new ByteArrayOutputStream();
+                } else if (line.contains(CERTIFICATE_PEM_SUFFIX)) {
+                    if (stream == null) {
+                        throw new RuntimeException("Could not parse certificate chain");
+                    }
+                    byte[] certificateBytes = Base64.getDecoder().decode(stream.toByteArray());
+                    X509Certificate x509Certificate = new X509Certificate("x509Certificate");
+                    x509Certificate.getParser().parse(new ByteArrayInputStream(certificateBytes));
+                    chain.addCertificate(x509Certificate);
+                    stream = null;
+                } else {
+                    try {
+                        if (stream == null) {
+                            throw new RuntimeException("Could not parse certificate chain");
+                        }
+                        stream.write(line.strip().getBytes());
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+        return chain;
     }
 }
