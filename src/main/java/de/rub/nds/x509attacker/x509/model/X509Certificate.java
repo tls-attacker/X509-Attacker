3113b4ae170e993eee2a5e35ca7e5ba1fd0fa4c7
Robert Merget <robert.merget@tii.ae>
2023-09-08 13:35:38 +0400

    updated license

--- src/main/java/de/rub/nds/x509attacker/x509/model/X509Certificate.java
+++ src/main/java/de/rub/nds/x509attacker/x509/model/X509Certificate.java
@@ -1,7 +1,7 @@
 /*
- * X509-Attacker - A tool for creating arbitrary certificates
+ * X.509-Attacker - A Library for Arbitrary X.509 Certificates
  *
- * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
@@ -293,11 +293,11 @@ public class X509Certificate extends Asn1Sequence implements X509Component {
     }
 
     public Boolean isExpired() {
-        return getNotAfter().isAfterNow();
+        return getNotAfter().isBeforeNow();
     }
 
     public Boolean isYetValid() {
-        return getNotBefore().isAfterNow();
+        return getNotBefore().isBeforeNow();
     }
 
     public Boolean isRevokedCrl() {
--- src/main/java/de/rub/nds/x509attacker/x509/model/extensions/BasicConstraints.java
+++ src/main/java/de/rub/nds/x509attacker/x509/model/extensions/BasicConstraints.java
@@ -1,7 +1,7 @@
 /*
- * X509-Attacker - A tool for creating arbitrary certificates
+ * X.509-Attacker - A Library for Arbitrary X.509 Certificates
  *
- * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
