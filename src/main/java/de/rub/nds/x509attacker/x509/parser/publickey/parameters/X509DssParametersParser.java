cbfe6260bfded09ddb38067cbee5ab04c6dcb700
Robert Merget <robert.merget@tii.ae>
2023-09-14 22:41:16 +0400

    added logger

--- src/main/java/de/rub/nds/x509attacker/x509/parser/publickey/parameters/X509DssParametersParser.java
+++ src/main/java/de/rub/nds/x509attacker/x509/parser/publickey/parameters/X509DssParametersParser.java
@@ -13,8 +13,12 @@ import de.rub.nds.x509attacker.chooser.X509Chooser;
 import de.rub.nds.x509attacker.x509.model.publickey.parameters.X509DssParameters;
 import de.rub.nds.x509attacker.x509.parser.X509ComponentContainerParser;
 import java.io.BufferedInputStream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class X509DssParametersParser extends X509ComponentContainerParser<X509DssParameters> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public X509DssParametersParser(X509Chooser chooser, X509DssParameters x509DssParameters) {
         super(chooser, x509DssParameters);
@@ -22,8 +26,24 @@ public class X509DssParametersParser extends X509ComponentContainerParser<X509Ds
 
     @Override
     protected void parseSubcomponents(BufferedInputStream inputStream) {
-        ParserHelper.parseAsn1Integer(encodable.getP(), inputStream);
-        ParserHelper.parseAsn1Integer(encodable.getQ(), inputStream);
+        LOGGER.debug("Parsing DssParameters");
+        parseP(inputStream);
+        parseQ(inputStream);
+        parseG(inputStream);
+    }
+
+    private void parseG(BufferedInputStream inputStream) {
         ParserHelper.parseAsn1Integer(encodable.getG(), inputStream);
+        LOGGER.debug("Parsing G: {}", encodable.getG().getValue().getValue());
+    }
+
+    private void parseQ(BufferedInputStream inputStream) {
+        ParserHelper.parseAsn1Integer(encodable.getQ(), inputStream);
+        LOGGER.debug("Parsing Q: {}", encodable.getQ().getValue().getValue());
+    }
+
+    private void parseP(BufferedInputStream inputStream) {
+        ParserHelper.parseAsn1Integer(encodable.getP(), inputStream);
+        LOGGER.debug("Parsing P: {}", encodable.getP().getValue().getValue());
     }
 }
