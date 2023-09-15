2fd00eb55b106541c06480862422f02a741df3d7
Robert Merget <robert.merget@tii.ae>
2023-09-15 09:34:15 +0400

    added logger

--- src/main/java/de/rub/nds/x509attacker/x509/parser/publickey/parameters/X509EcNamedCurveParametersParser.java
+++ src/main/java/de/rub/nds/x509attacker/x509/parser/publickey/parameters/X509EcNamedCurveParametersParser.java
@@ -10,12 +10,17 @@ package de.rub.nds.x509attacker.x509.parser.publickey.parameters;
 
 import de.rub.nds.asn1.parser.ParserHelper;
 import de.rub.nds.x509attacker.chooser.X509Chooser;
+import de.rub.nds.x509attacker.constants.X509NamedCurve;
 import de.rub.nds.x509attacker.x509.model.publickey.parameters.X509EcNamedCurveParameters;
 import de.rub.nds.x509attacker.x509.parser.X509ComponentParser;
 import java.io.BufferedInputStream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class X509EcNamedCurveParametersParser
         extends X509ComponentParser<X509EcNamedCurveParameters> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public X509EcNamedCurveParametersParser(
             X509Chooser chooser, X509EcNamedCurveParameters x509EcNamedCurveParameters) {
@@ -24,6 +29,13 @@ public class X509EcNamedCurveParametersParser
 
     @Override
     public void parse(BufferedInputStream inputStream) {
+        LOGGER.debug("Parsing X509EcNamedCurveParameters");
         ParserHelper.parseAsn1ObjectIdentifier(encodable, inputStream);
+        X509NamedCurve namedCurve =
+                X509NamedCurve.decodeFromOidBytes(encodable.getValueAsOid().getEncoded());
+        LOGGER.debug(
+                "Parsed ObjectIdentidifier: {} ({})",
+                encodable.getValue().getValue(),
+                namedCurve == null ? "unknown" : namedCurve.name());
     }
 }
