8c7198ec1b13ee248e872eb36df4ae5dc2675239
Felix Lange <felix.lange@uni-paderborn.de>
2025-05-15 10:33:10 +0200

    feat: implemented authority key identifier extension as needed by current anvil version

---
+++ src/main/java/de/rub/nds/x509attacker/config/extension/AuthorityKeyIdentifierConfig.java
@@ -0,0 +1,62 @@
+/*
+ * X.509-Attacker - A Library for Arbitrary X.509 Certificates
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.x509attacker.config.extension;
+
+import de.rub.nds.x509attacker.constants.GeneralNameChoiceType;
+import de.rub.nds.x509attacker.constants.X509ExtensionType;
+import de.rub.nds.x509attacker.x509.model.extensions.AuthorityKeyIdentifier;
+
+public class AuthorityKeyIdentifierConfig extends ExtensionConfig {
+
+    private byte[] keyIdentifier;
+    private GeneralNameChoiceType generalNameChoiceTypeConfig;
+    private Object generalNameConfigValue;
+    private int serialNumber;
+
+    public AuthorityKeyIdentifierConfig() {
+        super(X509ExtensionType.AUTHORITY_KEY_IDENTIFIER.getOid(), "authorityKeyIdentifier");
+    }
+
+    @Override
+    public AuthorityKeyIdentifier getExtensionFromConfig() {
+        return new AuthorityKeyIdentifier("authorityKeyIdentifier");
+    }
+
+    public byte[] getKeyIdentifier() {
+        return keyIdentifier;
+    }
+
+    public void setKeyIdentifier(byte[] keyIdentifier) {
+        this.keyIdentifier = keyIdentifier;
+    }
+
+    public GeneralNameChoiceType getGeneralNameChoiceTypeConfig() {
+        return generalNameChoiceTypeConfig;
+    }
+
+    public void setGeneralNameChoiceTypeConfig(GeneralNameChoiceType generalNameChoiceTypeConfig) {
+        this.generalNameChoiceTypeConfig = generalNameChoiceTypeConfig;
+    }
+
+    public Object getGeneralNameConfigValue() {
+        return generalNameConfigValue;
+    }
+
+    public void setGeneralNameConfigValue(Object generalNameConfigValue) {
+        this.generalNameConfigValue = generalNameConfigValue;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}
---
+++ src/main/java/de/rub/nds/x509attacker/x509/preparator/extension/AuthorityKeyIdentifierPreparator.java
@@ -0,0 +1,120 @@
+/*
+ * X.509-Attacker - A Library for Arbitrary X.509 Certificates
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.x509attacker.x509.preparator.extension;
+
+import de.rub.nds.asn1.model.Asn1Encodable;
+import de.rub.nds.asn1.preparator.Asn1PreparatorHelper;
+import de.rub.nds.x509attacker.chooser.X509Chooser;
+import de.rub.nds.x509attacker.config.extension.AuthorityKeyIdentifierConfig;
+import de.rub.nds.x509attacker.x509.model.extensions.AuthorityKeyIdentifier;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class AuthorityKeyIdentifierPreparator
+        extends ExtensionPreparator<AuthorityKeyIdentifier, AuthorityKeyIdentifierConfig> {
+    public AuthorityKeyIdentifierPreparator(
+            X509Chooser chooser,
+            AuthorityKeyIdentifier container,
+            AuthorityKeyIdentifierConfig config) {
+        super(chooser, container, config);
+    }
+
+    @Override
+    public void extensionPrepareSubComponents() {
+        // TODO: Probably refactor into Asn1 classes that hold context-specific tags
+
+        if (config.getKeyIdentifier() != null) {
+            Asn1PreparatorHelper.prepareField(field.getKeyIdentifier(), config.getKeyIdentifier());
+
+            // prepend original tag and length to content
+            field.getKeyIdentifier()
+                    .setContent(
+                            ArrayUtils.addAll(
+                                    field.getKeyIdentifier().getLengthOctets().getValue(),
+                                    field.getKeyIdentifier().getContent().getValue()));
+            field.getKeyIdentifier()
+                    .setContent(
+                            ArrayUtils.addAll(
+                                    field.getKeyIdentifier().getTagOctets().getValue(),
+                                    field.getKeyIdentifier().getContent().getValue()));
+
+            // set context-specific tag
+            field.getKeyIdentifier().setTagOctets(new byte[] {(byte) 0xa0});
+
+            // set outer length
+            field.getKeyIdentifier()
+                    .setLengthOctets(
+                            new byte[] {
+                                (byte) (field.getKeyIdentifier().getContent().getValue().length)
+                            });
+        }
+
+        // TODO: Preparator for GeneralNames
+
+        if (config.getSerialNumber() != 0) {
+            Asn1PreparatorHelper.prepareField(
+                    field.getAuthorityCertSerialNumber(),
+                    BigInteger.valueOf(config.getSerialNumber()));
+
+            // prepend original tag and length to content
+            field.getAuthorityCertSerialNumber()
+                    .setContent(
+                            ArrayUtils.addAll(
+                                    field.getAuthorityCertSerialNumber()
+                                            .getLengthOctets()
+                                            .getValue(),
+                                    field.getAuthorityCertSerialNumber().getContent().getValue()));
+            field.getAuthorityCertSerialNumber()
+                    .setContent(
+                            ArrayUtils.addAll(
+                                    field.getAuthorityCertSerialNumber().getTagOctets().getValue(),
+                                    field.getAuthorityCertSerialNumber().getContent().getValue()));
+
+            // set context-specific tag
+            field.getAuthorityCertSerialNumber().setTagOctets(new byte[] {(byte) 0xa2});
+
+            // set outer length
+            field.getAuthorityCertSerialNumber()
+                    .setLengthOctets(
+                            new byte[] {
+                                (byte)
+                                        (field.getAuthorityCertSerialNumber()
+                                                .getContent()
+                                                .getValue()
+                                                .length)
+                            });
+        }
+
+        field.getWrappingSequence().setContent(encodeSequenceContent());
+        Asn1PreparatorHelper.prepareAfterContent(field.getWrappingSequence());
+    }
+
+    @Override
+    public byte[] extensionEncodeChildrenContent() {
+        return encodeChildren(field.getWrappingSequence());
+    }
+
+    private byte[] encodeSequenceContent() {
+        List<Asn1Encodable> children = new ArrayList<>();
+
+        if (config.getKeyIdentifier() != null) {
+            children.add(field.getKeyIdentifier());
+        }
+
+        // TODO: General Names
+
+        if (config.getSerialNumber() != 0) {
+            children.add(field.getAuthorityCertSerialNumber());
+        }
+
+        return encodeChildren(children);
+    }
+}
